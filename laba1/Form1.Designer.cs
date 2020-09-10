2c6dc5bc75da43c12399dfb9aef47781ee1de8c5
herusanna <61598874+herusanna@users.noreply.github.com>
2020-09-10 19:14:46 +0300

    Добавьте файлы проекта.

---
+++ laba1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(62, 39);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 29);
+            this.inputBox.TabIndex = 0;
+
+            // 
+            // outputBox
+            // 
+            this.outputBox.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.Location = new System.Drawing.Point(312, 39);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(100, 29);
+            this.outputBox.TabIndex = 1;
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(185, 86);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(103, 28);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(185, 178);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(103, 27);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 228);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.inputBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ laba1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*if ((e.KeyChar <= 48 || e.KeyChar >= 59) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+                MessageBox.Show("Please, input number");
+            }
+            */
+        }
+        //получает обратную запись двоичного числа из дсятичного
+        private Int64 vvod(Int64 temp)
+        {
+            Int64 temp1 = 0;
+            List<Int64> s = new List<Int64>();
+            while (temp > 0)
+            {
+                temp1 = temp % 2;
+                temp = temp / 2;
+                s.Add(temp1);
+            }
+            return obrat(s);
+        }
+        //переворачивает число и возвращает прямую запись двоичного числа.
+        private Int64 obrat(List<Int64> norm)
+        {
+            Int64[] s = new Int64[norm.Count];
+            for (int i = norm.Count - 1; i >= 0; i--)
+            {
+                s[norm.Count - 1 - i] = norm[i];
+            }
+            return Convert.ToInt64(string.Join<Int64>("", s));
+        }
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (inputBox.Text.Length == 5)
+            {
+                int a = Convert.ToInt32(inputBox.Text);
+
+                outputBox.Text = vvod(a) + "";
+            }
+            else MessageBox.Show("error");
+
+
+        }
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
